eceffce41eaed3d9f8266c5f12cf21f836935e11
Olivier Beg <olivier@hadrian.io>
2024-07-02 18:02:42 +0200

    Remove garbage files

--- gixy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Find all .conf files and iterate over them
-find . -name '*.conf' | while IFS= read -r file; do
-    echo "$file"  # Echo the file name
-    gixy -f json "$file"  # Execute gixy on the file
-done
